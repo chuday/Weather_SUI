0706722047b2ecd21a36c90d96e5d0602a70c60c
chuday <mikhail_chudaew505@mail.ru>
2023-08-29 10:47:42 +0500

    Create app

---
+++ Weather_sui/Weather_sui/Weather_suiApp.swift
@@ -0,0 +1,17 @@
+//
+//  Weather_suiApp.swift
+//  Weather_sui
+//
+//  Created by Mikhail Chudaev on 29.08.2023.
+//
+
+import SwiftUI
+
+@main
+struct Weather_suiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
